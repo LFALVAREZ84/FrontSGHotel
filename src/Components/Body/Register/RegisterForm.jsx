801c21aed838703329339c4a0b3b3dbc3765047f
Luis Fabian Alvarez <lfalvarez84@outlook.com.ar>
2023-10-23 21:14:17 -0300

    Se creo el formulario registo

---
+++ src/Components/Body/Register/RegisterForm.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import axios from 'axios'; '../../Axios/axiosConfig'
+
+const RegisterForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    surname: '',
+    email: '',
+    pass: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('/api/users/register', formData, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.status === 201) {
+        console.log('Registro exitoso');
+        // Redirigir al usuario a la página de inicio de sesión si lo deseas
+      } else {
+        console.log('Error en el registro');
+      }
+    } catch (error) {
+      console.error('Error en la solicitud:', error);
+    }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className="mb-3">
+        <Form.Label>Nombre</Form.Label>
+        <Form.Control
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label>Apellido</Form.Label>
+        <Form.Control
+          type="text"
+          name="surname"
+          value={formData.surname}
+          onChange={handleChange}
+          required
+        />
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label>Email</Form.Label>
+        <Form.Control
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label>Contraseña</Form.Label>
+        <Form.Control
+          type="password"
+          name="pass"
+          value={formData.pass}
+          onChange={handleChange}
+          required
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Registrarse
+      </Button>
+    </Form>
+  );
+};
+
+export default RegisterForm;
